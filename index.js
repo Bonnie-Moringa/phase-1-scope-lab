63875fff7dfe85e37b0c4c21b211d152d1fdf268
Boniface Mburu <boniface.mburu@student.moringaschool.com>
2025-03-26 15:21:13 +0300

    Return customerName

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your solution in this file!
+// 1) Declare customerName in the global scope
+var customerName = "bob"; 
+
+//return customerName
+function returnCustomerName() {
+  return customerName;
+}
+
